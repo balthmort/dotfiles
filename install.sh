487ef37503c1fc6fdacd735d3cbe462a7961f6c3
balthmort <balthmort@gmail.com>
2015-12-27 01:26:37 +0100

    Minor corrections

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-git clone --recursive https://github.com/ballthmort/dotfiles.git
+git clone --recursive https://github.com/balthmort/dotfiles.git
 cd dotfiles && git remote 'set-url' --push origin git@github.com:balthmort/dotfiles.git
 
 exec ./setup.sh
