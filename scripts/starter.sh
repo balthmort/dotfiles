eac5abf1dc1e546b10c12026d459f0310cd741e6
balthmort <balthmort@gmail.com>
2016-01-11 21:37:35 +0100

    Little changes

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-tint2 
-xscreensaver -no-splash 
-lxterminal
-feh --bg-scale /usr/share/backgrounds/The-darkness-wall.jpg
-(sleep 2 && conky)
-(sleep 2 && firefox)
+tint2 &
+xscreensaver -no-splash &
+lxterminal &
+feh --bg-scale ~/system/dotfile/misc/The-darkness-wall.jpg &
+(sleep 2 && conky) &
+(sleep 2 && firefox) &
