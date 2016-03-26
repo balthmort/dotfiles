ed1361d65845f5d7dc85e20e6596a63f809653ca
balthmort <balthmort@gmail.com>
2016-03-26 04:13:18 +0100

    little tweaks

---
+++ latexmkrc
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+# Configuration file for latexmk, automation script for LaTeX builds
+
+# Use xelatex by default
+$pdf_mode = 1; $postscript_mode = $dvi_mode = 0;
+$pdflatex = "xelatex --shell-escape -src-specials -synctex=1 -interaction=nonstopmode %O %S";
--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 tint2 &
 xscreensaver -no-splash &
-lxterminal &
-feh --bg-scale ~/system/dotfile/misc/The-darkness-wall.jpg &
-(sleep 2 && conky) &
-(sleep 2 && firefox) &
+st &
+feh --bg-scale /home/mort/system/dotfiles/misc/The-darkness-wall.jpg &
+conky &
+firefox &
