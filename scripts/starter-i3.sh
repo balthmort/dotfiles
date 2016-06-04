b1656f65d4915fccc6b0f239877e20831f4c332d
Petr Surý <baltmort@gmail.com>
2016-06-04 10:53:03 +0200

    b

--- scripts/starter-i3.sh
+++ scripts/starter-i3.sh
@@ -3,6 +3,6 @@ i8kmon &
 st &
 feh --bg-scale /home/mort/system/dotfiles/misc/The-darkness-wall.jpg &
 nm-applet &
-setxkbmap -model pc104 -layout cz -option compose:lwin &
+setxkbmap -model pc104 -layout cz -option compose:lwin & #radsi pouzit localectl na nastavit to jen jednou
 redshift &
 firefox &
